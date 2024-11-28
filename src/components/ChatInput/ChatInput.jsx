28a9766b6bfc72dcd4856e13656248d8a4c0f1ea
nekoo7 <abylaiooaooa@mail.ru>
2024-11-29 02:48:37 +0500

    basic components and its styling

--- src/components/ChatInput/ChatInput.jsx
+++ src/components/ChatInput/ChatInput.jsx
@@ -11,3 +11,4 @@ class ChatInput extends Component {
         )
     }
 } 
+export default ChatInput
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,2 +1,9 @@
 import React, {Component} from 'react';
 import './Header.scss'
+
+const Header = () => {
+    <div className='header'>
+        <h2>Chat</h2> 
+    </div>
+}
+export default Header;
